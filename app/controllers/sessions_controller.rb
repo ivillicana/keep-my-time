684f180b4cd3c055f18680897a080f4954a52729
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-03-28 14:33:28 -0400

    create destroy action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,11 @@
 class SessionsController < ApplicationController
 
   def create
-    
+    binding.pry
   end
 
   def destroy
-    
+    session.delete :user_id
+    redirect_to new_session_path
   end
 end
