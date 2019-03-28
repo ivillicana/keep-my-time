e983624de45b3b3e2681efc6665b113b1491d132
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-03-28 19:33:16 -0400

    create collection select helper methods for reports

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -3,4 +3,23 @@ module ReportsHelper
   def report_month_year(report)
     "#{report.month} - #{report.year}"
   end
+
+  def report_months_for_select
+    %w[January
+      February
+      March
+      April
+      May
+      June
+      July
+      August
+      September
+      October
+      November
+      December]
+  end
+
+  def report_years_for_select
+    [2018, 2019, 2020]
+  end
 end
