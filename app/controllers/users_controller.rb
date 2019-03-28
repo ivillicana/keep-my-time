d4a218460bef42fea8a8173e41fd9d935db3a4b6
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-03-28 14:12:13 -0400

    create new and create actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    binding.pry
+    @user = User.create(user_params)
+    if @user.errors.any?
+      @errors = @user.errors.full_messages.join(", ")
+      render :new
+    end
+    session[:user_id] = @user.id
+    redirect_to user_path(@user), notice: "Welcome #{@user.first_name}! Account successfully created"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :telephone, :email, :password, :password_confirmation)
+  end
 end
