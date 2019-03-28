34605894cd0b4c96ee655230575fb5c4e05bc9ed
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-03-28 19:33:38 -0400

    create report_params method

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,2 +1,22 @@
 class ReportsController < ApplicationController
+
+  def index
+    @reports = current_user.reports
+  end
+
+  def new
+    
+  end
+
+  def create
+    current_user.reports.create(report_params)
+    redirect_to reports_path
+  end
+
+  private
+
+  def report_params
+    params.require(:report).permit(:month, :year, :placements, :video_showings, :hours, :return_visits, :bible_studies, :comments)
+  end
+  
 end
