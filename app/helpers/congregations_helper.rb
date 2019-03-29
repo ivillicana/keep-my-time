570dab91a2e20315e5bfda0df2feb761cf29f550
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-03-29 06:35:28 -0400

    create current_user_congregation method

--- app/helpers/congregations_helper.rb
+++ app/helpers/congregations_helper.rb
@@ -1,5 +1,11 @@
 module CongregationsHelper
+
   def current_user_congregation
-    current_user.congregation ? current_user.congregation.name : "No assigned congregation"
+    if current_user.congregation
+      current_user.congregation.name_with_city_state
+    else
+      render partial: 'congregations/list', locals: {user: current_user}
+    end
   end
+
 end
