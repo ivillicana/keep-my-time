755c7cec6bc32deb8d93e68ebc20aaebfb74017f
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-03-29 06:38:59 -0400

    create name_with_city_state

--- app/models/congregation.rb
+++ app/models/congregation.rb
@@ -1,4 +1,8 @@
 class Congregation < ApplicationRecord
   has_many :users
   has_many :reports, through: :users
+
+  def name_with_city_state
+    "#{self.name} - #{self.city}, #{self.state}"
+  end
 end
